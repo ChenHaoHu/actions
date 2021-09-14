5106e9a000a9e59cdb4222a4355419ed2f4a2fa4
huchenhao <chenhaohu1998@sina.com>
2021-09-14 21:56:17 +0800

    Update wechat_notice.sh

--- wechat_notice.sh
+++ wechat_notice.sh
@@ -1,13 +1,15 @@
-
-curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=a27d4dc9-c0ec-40fb-82c6-525f67b338b2' \
-   -H 'Content-Type: application/json' \
-   -d '
+content='
    {
     "msgtype": "markdown",
     "markdown": {
-        "content": "实时新增用户反馈<font color=\"warning\">132例</font>，请相关同事注意。\n
-         >类型:<font color=\"comment\">用户反馈</font>
-         >普通用户反馈:<font color=\"comment\">117例</font>
-         >VIP用户反馈:<font color=\"comment\">15例</font>"
+        "content": "啥时候开始干饭"
     }
-}'
+}';
+
+
+echo $content;
+
+curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='$1 \
+    -v \
+   -H 'Content-Type: application/json' \
+   -d "$content"
